ebe91cf3d5f6a2905eedda4819886549d840d88f
rohanghosh <rohangho@live.com>
2022-06-02 21:40:00 +0530

    updated 947 test code

--- src/com/company/LeetCode947.kt
+++ src/com/company/LeetCode947.kt
@@ -2,68 +2,90 @@ package com.company
 
 import java.util.*
 import kotlin.collections.ArrayList
+import kotlin.math.min
 
 object LeetCode947 {
     @JvmStatic
     fun main(args: Array<String>) {
        println(
-           removeStones( arrayOf(intArrayOf(0,1),
-           intArrayOf(1,2),
-           intArrayOf(1,3),
-           intArrayOf(3,3),
-           intArrayOf(2,3),
-               intArrayOf(0,2),
+           removeStones( arrayOf(intArrayOf(4,4),
+           intArrayOf(5,5),
+           intArrayOf(3,1),
+           intArrayOf(1,4),
+           intArrayOf(1,1),
+               intArrayOf(2,3),
+               intArrayOf(0,3),
+               intArrayOf(2,4),
+               intArrayOf(3,5),
            )))
     }
 
 
-    fun removeStones(stones: Array<IntArray>): Int {
-        var noofRow =0
-        var noOfColumn = 0
+    private fun removeStones(stones: Array<IntArray>): Int {
+        if(stones.size>1) {
+            var noofRow = 0
+            var noOfColumn = 0
 
-        var rowArray:ArrayList<Int> = ArrayList()
-        var columnArray:ArrayList<Int> =ArrayList()
+            var rowArray: ArrayList<Int> = ArrayList()
+            var columnArray: ArrayList<Int> = ArrayList()
 
-        for(i in stones.indices)
-        {
-            if(noofRow<stones[i][0])
-                noofRow = stones[i][0]
-            if(noOfColumn<stones[i][1])
-                noOfColumn = stones[i][1]
-        }
-
-        for(i in 0..noofRow)
-            rowArray.add(0)
-        for(i in 0..noOfColumn)
-            columnArray.add(0)
-
-
-        for (i in stones.indices)
-            rowArray[stones[i][0]] = rowArray[stones[i][0]] +1
-
-
-        for (i in stones.indices)
-            columnArray[stones[i][1]] = columnArray[stones[i][1]] +1
-
-
-        var counter = 0
-        for(i in stones.indices)
-        {
-            if(rowArray[stones[i][0]]>1 )
-            {
-                rowArray[stones[i][0]] =  rowArray[stones[i][0]]-1
-                columnArray[stones[i][1]] =  columnArray[stones[i][1]]-1
-                counter += 1
-            }
-            else if( columnArray[stones[i][1]]>1)
-            {
-                columnArray[stones[i][1]] =  columnArray[stones[i][1]]-1
-                rowArray[stones[i][0]] =  rowArray[stones[i][0]]-1
-                counter += 1
+            for (i in stones.indices) {
+                if (noofRow < stones[i][0])
+                    noofRow = stones[i][0]
+                if (noOfColumn < stones[i][1])
+                    noOfColumn = stones[i][1]
             }
 
+            for (i in 0..noofRow)
+                rowArray.add(0)
+            for (i in 0..noOfColumn)
+                columnArray.add(0)
+
+            var globalCounter = 1
+
+            for(i in stones.indices)
+            {
+                if(rowArray[stones[i][0]] == 0)
+                {
+                    rowArray[stones[i][0]] = globalCounter
+                    globalCounter ++
+                }
+                if(columnArray[stones[i][1]] == 0)
+                {
+                    columnArray[stones[i][1]] = globalCounter
+                    globalCounter ++
+                }
+            }
+
+            var counter =0
+            while (counter<2) {
+                for (i in stones.indices) {
+                    val min = Math.min(rowArray[stones[i][0]], columnArray[stones[i][1]])
+                    rowArray[stones[i][0]] = min
+                    columnArray[stones[i][1]] = min
+                }
+                counter++
+            }
+
+
+            return  stones.size - (findUnique(rowArray,columnArray).toInt())
+
         }
-        return counter
+        else
+            return 0
 
     }
+
+    private fun findUnique(rowArray: ArrayList<Int>, columnArray: ArrayList<Int>): Long {
+        var newJoinArray: ArrayList<Int> = ArrayList()
+        newJoinArray = rowArray
+        newJoinArray.addAll(columnArray)
+
+        newJoinArray = newJoinArray.filter { it>0 } as ArrayList<Int>
+        return newJoinArray.stream().distinct().count()
+
+    }
+
+
+
 }
