148e2c7c1dc05a427dee4eb0b1fccf39bde6ef99
rohanghosh <rohangho@live.com>
2022-06-03 00:25:27 +0530

    added the 657 working code

--- src/com/company/LeetCode657.kt
+++ src/com/company/LeetCode657.kt
@@ -1,8 +1,32 @@
 package com.company
 
+import java.util.Stack
+
 object LeetCode657 {
     @JvmStatic
     fun main(args: Array<String>) {
-        println("Ho")
+        println(judgeCircle("LL"))
+    }
+
+    fun judgeCircle(moves: String): Boolean {
+
+        var leftCounter =0
+        var rightCounter =0;
+        var upCounter =0;
+        var downCounter =0;
+        val movesInChar = moves.toCharArray()
+        for(i in movesInChar.indices)
+        {
+           if(movesInChar[i] == 'L')
+               leftCounter += 1
+            if(movesInChar[i] == 'R')
+                rightCounter += 1
+            if(movesInChar[i] == 'U')
+                upCounter += 1
+            if(movesInChar[i] == 'D')
+                downCounter += 1
+        }
+
+        return leftCounter == rightCounter && upCounter == downCounter
     }
 }
