5cfb1923c19e59ca3b90a7b63921fc19de733457
rohanghosh <rohangho@live.com>
2022-06-05 01:12:56 +0530

    added code 717

--- src/com/company/LeetCode717.kt
+++ src/com/company/LeetCode717.kt
@@ -4,6 +4,29 @@ object LeetCode717 {
 
     @JvmStatic
     fun main(args: Array<String>) {
-        println("hello")
+        println(isOneBitCharacter(intArrayOf(1,1,1,0)))
+    }
+
+    fun isOneBitCharacter(bits: IntArray): Boolean {
+        var i =0
+        var output = false
+        while (true)
+        {
+            if(i==bits.size-1) {
+                output = true
+                break
+            }
+            if(i >bits.size-1)
+            {
+                output = false
+                break
+            }
+
+            i += if( bits[i] == 0)
+                1
+            else
+                2
+        }
+        return output;
     }
 }
