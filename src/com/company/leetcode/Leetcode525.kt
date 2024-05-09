0b7ae72a2ad762e6db515e1a2a8bd0444cdbd144
rohanghosh <rohangho@live.com>
2024-05-09 19:26:44 +0530

    added 525

---
+++ src/com/company/leetcode/Leetcode525.kt
@@ -0,0 +1,40 @@
+package com.company.leetcode
+
+import kotlin.math.max
+
+object Leetcode525 {
+
+    @JvmStatic
+    fun main(args: Array<String>) {
+        print(findMaxLength(intArrayOf(0, 0, 0, 1, 1, 1, 0)))
+    }
+
+    fun findMaxLength(nums: IntArray): Int {
+        val countArray = mutableListOf<Int>()
+        var sum = 0;
+        for (i in nums.indices) {
+            sum += if (nums[i] == 0)
+                -1
+            else
+                1
+            countArray.add(sum)
+        }
+        var longest = Int.MIN_VALUE
+        val map = mutableMapOf<Int, Int>()
+
+        for (i in countArray.indices) {
+            if (countArray[i] == 0)
+                longest = max(longest, i + 1)
+            else {
+                if (map.contains(countArray[i]))
+                    longest = max(longest, i - map[countArray[i]]!!)
+                else
+                    map[countArray[i]] = i
+            }
+        }
+        return if (longest >= 0)
+            longest
+        else
+            0
+    }
+}
